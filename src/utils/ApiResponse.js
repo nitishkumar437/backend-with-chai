65e498623aa6a5f6d02757a5ac80ed187dc86aec
Nitish Kumar <ns8071287@gmail.com>
2026-04-19 21:07:42 +0530

    add register controoler

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -6,3 +6,4 @@ class ApiResponse {
     this.successs = satusCode < 400;
   }
 }
+export { ApiResponse }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath);
+    return null;
   }
 };
-
-export { cloudinary };
+export { uploadOnCloudinary };
